564cf92c68ddcdf8f38811f0e855077d4b414bb7
TheAlgorythm <36081522+TheAlgorythm@users.noreply.github.com>
2019-03-28 09:30:50 +0100

    added time elapsing

--- App.py
+++ App.py
@@ -1,20 +1,27 @@
 import sys
 from bitarray import bitarray
 from Encryption import Encryption
+from datetime import datetime
 
 
 class App:
     @staticmethod
     def exec(argv):
         message = bitarray()
-        #message.fromstring(argv[1])
         with open(argv[1], 'rb') as fh:
             message.fromfile(fh)
+
         key = bitarray()
-        #key.fromstring(argv[2])
         with open(argv[2], 'rb') as fh:
             key.fromfile(fh)
+        
+        start_time = datetime.now()
+
         encrypted = Encryption.encrypt(message, key)
+
+        time_elapsed = datetime.now() - start_time
+        print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
+
         with open(argv[3], 'wb') as fh:
             encrypted.tofile(fh)
 
