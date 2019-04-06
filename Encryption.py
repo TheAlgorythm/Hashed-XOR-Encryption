b69678079deda9f740718ce62243ac583ae3c29a
TheAlgorythm <36081522+TheAlgorythm@users.noreply.github.com>
2019-04-06 16:13:17 +0200

    better Style

--- Encryption.py
+++ Encryption.py
@@ -6,20 +6,19 @@ import math
 class Encryption:
 
     @staticmethod
-    def encrypt(message, key, difficulty=2):
+    def encrypt(message: bitarray, key: bitarray, difficulty=2) -> bitarray:
         cypher = bitarray()
         keyLength = 128
         blockSize = int(keyLength / difficulty)
         blocks = Encryption.sliceToBlocks(message, blockSize)
         for block in blocks:
-            newKey = bitarray()
-            newKey.frombytes(Encryption.keyDerivation(key, keyLength))
+            newKey = Encryption.keyDerivation(key, keyLength)
             Encryption.addToList(cypher, block ^ newKey[:block.length() * difficulty:difficulty])
             key = newKey
         return cypher
     
     @staticmethod
-    def sliceToBlocks(iterList, blockSize):
+    def sliceToBlocks(iterList: bitarray, blockSize: int) -> list:
         blocks = list()
         listLength = len(iterList)
         blockCount = math.ceil(listLength / blockSize)
@@ -31,8 +30,10 @@ class Encryption:
         return blocks
 
     @staticmethod
-    def keyDerivation(oldKey, length):
-        return hashlib.md5(oldKey.tobytes()).digest()
+    def keyDerivation(oldKey: bitarray, length: int) -> bitarray:
+        newKey = bitarray()
+        newKey.frombytes(hashlib.md5(oldKey.tobytes()).digest())
+        return newKey
 
     @staticmethod
     def addToList(iterList, appending):
